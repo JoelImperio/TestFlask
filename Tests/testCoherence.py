8963e0b3f65c307fb2b7c541f2927d90ec357acc
rob1moz <50520555+rob1moz@users.noreply.github.com>
2019-07-14 20:11:57 +0200

    upDate structure

--- Tests/testCoherence.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jul 14 18:47:10 2019
-
-@author: Robin
-"""
-
-#- Vérifier la cohérence des Inputs
-#- Vérifier la cohérence du Portfolio: les MP le nombre de polices
