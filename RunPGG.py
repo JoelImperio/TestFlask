a73656664a25eadf395476a0ab5e477fcdf33382
rob1moz <50520555+rob1moz@users.noreply.github.com>
2019-07-14 21:23:15 +0200

    Update RunPGG.py

--- RunPGG.py
+++ RunPGG.py
@@ -11,3 +11,12 @@ Created on Sun Jul 14 18:35:51 2019
 #-	Défini les groupement de produits
 
 
+# Export les inputs pour 1 run 
+# Export le portfolio
+# 
+# for allRuns:
+#     For allPorfolio:
+#         Run all policies (avec un case statement sur la modalité qui appel toutes les fonctions de produits)
+#    return les valeurs agrégée
+#
+#Faire les agrégations
