88c73f95742fc32a947affe5a58e445dc842f964
robinWengi <52786448+robinWengi@users.noreply.github.com>
2019-08-26 09:18:21 +0200

    c

--- ModX.py
+++ ModX.py
@@ -7,3 +7,8 @@ Created on Sun Jul 14 18:46:08 2019
 #- les produits sont constitué uniquement d'appel de fonctions
 #- Il s'agit d'une fonction qui prend en Input le set de parametre pour la police
 #- Il prend également le set de paramètre généraux et particulier au numéro de Run
+
+def ModX(Portfolio,hypo):
+    
+    
+    return dfCashFlow
