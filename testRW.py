5212853077b28f172e4153316ec6860c8c0d85db
rob1moz <50520555+rob1moz@users.noreply.github.com>
2019-08-28 01:59:41 +0200

    a

--- testRW.py
+++ testRW.py
@@ -1,5 +1,6 @@
 from Portefeuille.Portfolio import portfolio
 from Hypotheses.Hypothesis import hypo
+import numpy as np
 
 a=portfolio.ids([301,2501])
 b=portfolio.mod([8,9])
@@ -9,3 +10,7 @@ hyp=hypo([2,5])
 d=hyp.lapse()
 e=hyp.run
 
+f=a.iloc[:,1:5].to_numpy()
+g=np.ones([2,1])
+h=f+g
+i=h+1
