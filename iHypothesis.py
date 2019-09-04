3d52886a267217b707d6f3c9e9496fd6ca55451a
rob1moz <50520555+rob1moz@users.noreply.github.com>
2019-09-04 23:59:33 +0200

    rw

--- iHypothesis.py
+++ iHypothesis.py
@@ -1,14 +1,19 @@
 import pandas as pd
 import numpy as np
 import os, os.path
+import ast
 path = os.path.dirname(os.path.abspath(__file__))
-
+ 
 
 #Chargement des fichiers inputs contenant les hypothèses
 h=pd.ExcelFile(path  + '/Hypotheses/TablesProphet 2018-12.xls').parse("Hypothèses")
 h1=pd.ExcelFile(path  + '/Hypotheses/TablesProphet 2018-12.xls').parse("Hypothèses")
 
- 
+def eval_code(code):
+    parsed = ast.parse(code, mode='eval')
+    fixed = ast.fix_missing_locations(parsed)
+    compiled = compile(fixed, '<string>', 'eval')
+    eval(compiled)
 
 #Création de la class Portefeuille
 
@@ -25,10 +30,36 @@ class Hypo:
         else:
             self.h=hy1
  
-    def fraisGestion(self):
-        coutParPolice=self.h.iloc[43,3]
+    def ifsRun(self,choiceForEachRun):
         
-        return coutParPolice
+        adminCost=self.h.iloc[43,3]*(self.un)
+        SecurityMarginMarge=1+self.h.iloc[47,2]
+        SecurityMarginBio=1+self.h.iloc[48,2]
+        
+        
+        c=self.un
+        listeDesRuns=self.run
+        
+        for i in range(len(listeDesRuns)):
+            z=listeDesRuns[i]
+            z=z*(self.un)
+            condlist = [z[:,:,i]==0,z[:,:,i]==1,z[:,:,i]==2,z[:,:,i]==3,z[:,:,i]==4,z[:,:,i]==5]
+            c[:,:,i]=np.select(condlist, eval(choiceForEachRun))
+        return c
+    
+    def fraisGestion(self):
+        
+        choiceForEachRun='[adminCost[:,:,i],adminCost[:,:,i],adminCost[:,:,i],adminCost[:,:,i],adminCost[:,:,i]*SecurityMarginMarge,adminCost[:,:,i]*SecurityMarginBio]'
+        
+        return self.ifsRun(choiceForEachRun)
+    
+    
+    def fraisGestionPlacement(self):
+    
+        choiceForEachRun='[adminCost[:,:,i],adminCost[:,:,i],adminCost[:,:,i],adminCost[:,:,i],adminCost[:,:,i]*SecurityMarginMarge,adminCost[:,:,i]*SecurityMarginBio]'
+    
+        return self.ifsRun(choiceForEachRun)
+
 
 
 
@@ -40,7 +71,8 @@ class Hypo:
 
 ##############################ICI pour faire des tests sur la class##########################################################
 from iPortfolio import Portfolio
-myRun=[1,5]
+#myRun=[1,5]
+myRun=[0,1,2,3,4,5]
 policies=Portfolio(runs=myRun)
 policies.mod([8,9])
 shape=policies.shape
@@ -48,31 +80,19 @@ shape=policies.shape
 hyp=Hypo(MyShape=shape, Run=myRun)
 
 a=hyp.fraisGestion()
-b=hyp.un
-
-for i in range(len(hyp.run)):
-    
-    z=(hyp.run)[i]
-    if z==0:
-        b[:,:,i]=b[:,:,i]*a*i
-    elif z==1:
-        b[:,:,i]=b[:,:,i]*a*i
-    elif z==2:
-        b[:,:,i]=b[:,:,i]*a*i
-    elif z==3:
-        b[:,:,i]=b[:,:,i]*a*i
-    elif z==4:
-        b[:,:,i]=b[:,:,i]*a*i
-    elif z==5:
-        b[:,:,i]=b[:,:,i]*a*i
+b=hyp.fraisGestionPlacement()
 
 
-#np.where(a==1,) or np.select
-#https://medium.com/@emayoung95/using-numpy-where-function-to-replace-for-loops-with-if-else-statements-a1e6044ac4c1
-#https://stackoverflow.com/questions/39109045/numpy-where-with-multiple-conditions/39111919
-c=hyp.un
-c[:,:,1]=np.where(b[:,:,1]==a,b[:,:,1]*2,0)
-    
+
+
+#c=hyp.un 
+#for i in range(len(hyp.run)):
+#    z=(hyp.run)[i]
+#    z=z*b
+#    condlist = [z[:,:,i]==0,z[:,:,i]==1,z[:,:,i]==2,z[:,:,i]==3,z[:,:,i]==4,z[:,:,i]==5]
+#    choicelist = [b[:,:,i]*a*i,b[:,:,i]*a*i,b[:,:,i]*a*i,b[:,:,i]*a*i,b[:,:,i]*a*i,b[:,:,i]*a*i]
+#    c[:,:,i]=np.select(condlist, choicelist)
+
 
 #b=a.iloc[1:,1].to_numpy()
 #c=np.repeat(b,12)
@@ -88,7 +108,7 @@ c[:,:,1]=np.where(b[:,:,1]==a,b[:,:,1]*2,0)
 #    - Sinistralité
 #    - Lapse
 #    - Reduction
-#    - frais Gestion
+#    - frais Gestion v
 #    - frais gestion placement
 #    - Commissions
 
