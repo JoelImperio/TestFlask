9d03f149ae52d03e356944c12afb1ef01be1b382
JoelImperio <52786600+JoelImperio@users.noreply.github.com>
2019-09-12 13:33:32 +0200

    Update PDFGenerator.bat

--- Documentation pdf/PDFGenerator.bat
+++ Documentation pdf/PDFGenerator.bat
@@ -1,1 +1,1 @@
-a: & cd 01 - Reserving\PGG\PGGenPython\JOmyPGG\MyPGG1\Documentation pdf & pandoc README.md -o testpdf.pdf
+a: & cd 01 - Reserving\PGG\PGGenPython\JOmyPGG\MyPGG1\Documentation pdf & pandoc RapportPGG.md -o testpdf.pdf
