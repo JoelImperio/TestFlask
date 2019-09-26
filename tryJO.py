18cccb147f08ae6edf725e72ad4d9a22d9d6b947
JoelImperio <52786600+JoelImperio@users.noreply.github.com>
2019-09-26 22:36:45 +0200

    jo

--- tryJO.py
+++ tryJO.py
@@ -1,1 +1,14 @@
-test
+import numpy as np
+import pandas as pd
+import time
+
+from Portefeuille import Portfolio
+from Parametres import Hypo
+
+from MyPyliferisk import MortalityTable,qx
+from MyPyliferisk.mortalitytables import EKM05i
+
+
+tariff = MortalityTable(nt=EKM05i)
+
+print(tariff.qx[50])
