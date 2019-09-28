3e9c35ae15876d4e3db47f765062b86369830da6
rob1moz <50520555+rob1moz@users.noreply.github.com>
2019-09-29 00:49:39 +0200

    Update Produits.py

--- Produits.py
+++ Produits.py
@@ -1,39 +1,24 @@
 from Portefeuille import Portfolio
 from Parametres import Hypo
-import numpy as np
+
 import pandas as pd
+import numpy as np
+from MyPyliferisk import MortalityTable
+from MyPyliferisk.mortalitytables import EKM05i
 
-from MyPyliferisk import MortalityTable,vqx
-from MyPyliferisk.mortalitytables import EKM95, EKF95
+import os, os.path
+path = os.path.dirname(os.path.abspath(__file__))
 
-
-tariff = MortalityTable(nt=EKM95)
-
-
-
-
+#Ceci est une class d'exemple pour reprendre merci de ne pas toucher
 class FU(Portfolio):
     mods=[8,9]
-    
     
     def __init__(self):
         super().__init__()
         self.p=self.mod(self.mods)
-        
 
-    def inforce(self):
-        
-        tariff2 = np.vectorize(qx)
-        
-        rqx = self.age1()
-        
-        return rqx
 
 sp=FU()       
 b=sp.un
 c=sp.vide
-#d=sp.p
 
-z = FU.inforce(policies)
-
-#print(tariff.qx[10])
