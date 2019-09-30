4921304c310f6bc6fd4b602158b94e75e6caddd4
JoelImperio <52786600+JoelImperio@users.noreply.github.com>
2019-09-30 09:14:22 +0200

    Update Produits.py

--- Produits.py
+++ Produits.py
@@ -1,5 +1,6 @@
 from Portefeuille import Portfolio
 from Parametres import Hypo
+<<<<<<< HEAD
 import numpy as np
 import pandas as pd
 
@@ -11,15 +12,22 @@ tariff = MortalityTable(nt=EKM95)
 
 
 
+import pandas as pd
+import numpy as np
+from MyPyliferisk import MortalityTable
+from MyPyliferisk.mortalitytables import EKM05i
 
+import os, os.path
+path = os.path.dirname(os.path.abspath(__file__))
+
+#Ceci est une class d'exemple pour reprendre merci de ne pas toucher
 class FU(Portfolio):
     mods=[8,9]
-    
     
     def __init__(self):
         super().__init__()
         self.p=self.mod(self.mods)
-        
+
 
 
 #Il faut vectorisé ici la fonction des qx
@@ -34,11 +42,12 @@ class FU(Portfolio):
 
 
 
+
 sp=FU()       
 b=sp.un
 c=sp.vide
+<<<<<<< HEAD
 #d=sp.p
 bbb = FU.inforce(policies)
 z = FU.inforce(policies)
 
-#print(tariff.qx[10])
