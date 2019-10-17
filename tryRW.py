0e01496d48a36e67b567ae88e55e2b4216d8e9bd
robinWengi <52786448+robinWengi@users.noreply.github.com>
2019-10-17 13:59:05 +0200

    rw

--- tryRW.py
+++ tryRW.py
@@ -23,6 +23,7 @@ class myHypo(Hypo):
 
         def __init__(self):
             super().__init__()
+            
 
 
 
@@ -32,17 +33,41 @@ class myFU(Portfolio):
     def __init__(self):
         super().__init__()
         self.p=self.mod(self.mods)
+        self.ageMax=65
 
 
+    def polTermM(self):
+        
+        entryAge1= self.p['Age1AtEntry'].to_numpy()
+        
+        entryAge2=self.p['Age2AtEntry'].to_numpy()
+        
+        entryAge2[entryAge2==999]=0
+        ageAtEntry=np.maximum(entryAge1,entryAge2)
+        
+        #Nous pensons que cette variante est plus correct car dans le mod 9 la police continue jusqu'à 65 ans du plus jeune assuré
+        #Il faut ajouté le code commenté pour prendre en compte le changement
+#        mod=self.p['PMBMOD'].to_numpy()        
+#        entryAge2[entryAge2==0]=999
+#        ageAtEntry[mod==9]=np.minimum(entryAge1[mod==9],entryAge2[mod==9])
+        
+        
+        ageAtEntry=ageAtEntry[:,np.newaxis,np.newaxis]*self.un
+        
+        ageTerm=self.ageMax*self.un
+        
+        polTerm=(ageTerm-ageAtEntry)*12
 
-
+        return polTerm
 
 pol=myFU()
 hyp=myHypo()
 
+z=pol.ids([1735601])
+#z=pol.mod([9])
 
 
-
+a=pol.polTermM()
 
 
 
