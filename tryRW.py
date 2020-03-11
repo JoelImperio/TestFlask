e3dca7105448fdf679026cfbce5a40b172fdec38
robinWengi <52786448+robinWengi@users.noreply.github.com>
2020-03-11 17:29:34 +0100

    rw

--- tryRW.py
+++ tryRW.py
@@ -14,9 +14,10 @@ start_time = time.time()
 #Création de la class Epargne
 ############################################################################################################################
 
-class EP(Portfolio):
-    mods=[28,29,30,31,32,33,36]
-    ageLimite = 75    
+class MI(Portfolio):
+    mods=[2,10,6,7]
+#Products: F1XT_1,F2XT_1,F1XT14,F1XT11
+
 
     
     def __init__(self,run=allRuns,\
@@ -29,10 +30,10 @@ class EP(Portfolio):
 #Permet de relancer l'update() en intégrant des methodes de la sous-classe
     def update(self,subPortfolio):
         super().update(subPortfolio)
-        self.loopSaving()
+        self.loopEndowment()
 
-#Cette Loop renvoie l'ensemble des variables récusrives pour les produits épargnes
-    def loopSaving(self):
+#Cette Loop renvoie l'ensemble des variables récusrives pour les produits mixtes
+    def loopEndowment(self):
 
 #Variables des actifs            
         nbrPolIf=self.one()
@@ -43,27 +44,7 @@ class EP(Portfolio):
         nbrNewRed = self.zero()
         matRate=self.zero()
         
-#Variables des réduites
-        nbrPupsIf=self.zero()
-        nbrPupDeath=self.zero()
-        nbrPupSurrender=self.zero()
-        nbrPupIfSM=self.zero()
-        nbrPupMaturities=self.zero()
 
-#Variables de l'épargne actifs et réduits
-        epargnAcquPP = self.zero()        
-        eppAcquAPPUP = self.zero()
-        epAcquAVPUP = self.zero()
-
-        epargnAcquPP[:,0,:] = self.p['PMBPRVMAT'].to_numpy()[:,np.newaxis]        
-
-
-#Variables de PB actifs et réduites
-        pupBBenPP = self.zero()
-        pbAcquAVPUP = self.zero()
-        pbAcquAPPUP =self.zero()
-        
-        pupBBenPP[:,0,:] = self.p['PMBPBEN'].to_numpy()[:,np.newaxis]
       
         
 #Variables biométriques et génériques
@@ -74,8 +55,7 @@ class EP(Portfolio):
         reduction = self.reduction()       
         qxy=self.qxyExpMens()
         qxyD =lapseTiming * self.qxyExpMens()
-        txInteret = self.txInt()
-        prEncInv = self.premiumInvested()
+
         
         #Définition du vecteur des maturités (bool)        
         matRate[polTermM+1 ==self.durationIf()]=1 
@@ -95,35 +75,6 @@ class EP(Portfolio):
             
             nbrPolIf[:,i,:]=nbrPolIf[:,i-1,:]-nbrDeath[:,i,:]-nbrSurrender[:,i,:]- nbrNewRed[:,i,:] - nbrMaturities[:,i,:]
 
-#Définition des variables des réduites
-            nbrPupMaturities[:,i,:]=nbrPupsIf[:,i-1,:]*matRate[:,i,:]
-            
-            nbrPupIfSM[:,i,:]=nbrPupsIf[:,i-1,:] - nbrPupMaturities[:,i,:]
-            
-            nbrPupDeath[:,i,:]=nbrPupIfSM[:,i,:]*qxy[:,i,:]*(1-(lapseTiming*lapse[:,i,:]))
-            
-            nbrPupSurrender[:,i,:]=nbrPupIfSM[:,i,:]*lapse[:,i,:]*(1-(lapseTiming*qxy[:,i,:]))
-            
-            nbrPupsIf[:,i,:]=nbrPupsIf[:,i-1,:]-nbrPupDeath[:,i,:]-nbrPupSurrender[:,i,:] - nbrPupMaturities[:,i,:] + nbrNewRed[:,i,:]
-
-#Définition des variables d'épargne pour actives et réduites
-            epargnAcquPP[:,i,:]= (epargnAcquPP[:,i-1,:] + prEncInv[:,i,:]) * txInteret[:,i,:]
-
-            epAcquAVPUP[:,i,:] = eppAcquAPPUP[:,i-1,:] * txInteret[:,i,:]
-            
-            epTemp=epAcquAVPUP[:,i,:] * (nbrPupsIf[:,i,:] - nbrNewRed[:,i,:]) + epargnAcquPP[:,i,:] * nbrNewRed[:,i,:]
-            
-            eppAcquAPPUP[:,i,:]=np.divide(epTemp,nbrPupsIf[:,i,:],out=np.zeros_like(epTemp), where=nbrPupsIf[:,i,:]!=0)
-
-#Définition des variables de PB pour actives et réduites
-            pupBBenPP[:,i,:] = pupBBenPP[:,i-1,:] * txInteret[:,i,:]
-            
-            pbAcquAVPUP[:,i,:] = pbAcquAPPUP[:,i-1,:] * txInteret[:,i,:]
-            
-            pbTemp=pbAcquAVPUP[:,i,:] * (nbrPupsIf[:,i,:] - nbrNewRed[:,i,:]) + pupBBenPP[:,i,:] * nbrNewRed[:,i,:]            
-            
-            pbAcquAPPUP[:,i,:] = np.divide(pbTemp,nbrPupsIf[:,i,:],out=np.zeros_like(pbTemp), where=nbrPupsIf[:,i,:]!=0)       
-
 
 #Sauvegarde des variables des actifs
        
@@ -139,122 +90,13 @@ class EP(Portfolio):
         self.nbrNewRed = nbrNewRed
         # Nombre de nouvelle maturités
         self.nbrNewMat = nbrMaturities
-        
-#Sauvegarde des variables des réduites
-        
-        #Nombre de polices réduites                                
-        self.nbrPupsIf=nbrPupsIf
-        #Nombre de police réduites en déduisant les échéances du mois
-        self.nbrPupIfSM=nbrPupIfSM
-        #Nombre de décès de polices résuites
-        self.nbrPupDeath=nbrPupDeath
-        #Nombre d'annulation de contrat de polices réduites
-        self.nbrPupSurrender=nbrPupSurrender
-        # Nombre de police réduite arrivant à maturité
-        self.nbrPupMat = nbrPupMaturities
-
-#Sauvegarde des variables d'éparnge actifs et réduites
-        
-        #Epargne acquise par police AVANT nouvelle réduction                                 
-        self.epAcquAVPUP=epAcquAVPUP
-        #Epargne acquise par police APRES nouvelle réduction 
-        self.eppAcquAPPUP=eppAcquAPPUP
-        #Epargne aquise des polices actives
-        self.epargnAcquPP = epargnAcquPP
-
-
-#Sauvgarde des variables de  PB pour actives et réduites
-        
-        # PB acquise par police AVANT nouvelle réduction                                 
-        self.pbAcquAVPUP=pbAcquAVPUP
-        # PB acquise par police APRES nouvelle réduction 
-        self.pbAcquAPPUP=pbAcquAPPUP
-        # PB acquise des polices actives
-        self.pbAcquPP = pupBBenPP      
+             
         
         return
 
-#Retourne les primes commerciales annuel  avec indexation
-    def premiumAnnual(self):
-        
-        txIndex = (self.p['POLINDEX'].to_numpy()[:,np.newaxis,np.newaxis]/100) * self.one()
-       
-        premAnn = self.p['POLPRTOT'].to_numpy()[:,np.newaxis,np.newaxis] * self.one()
-        
-        return  premAnn * (1 + txIndex)**self.projectionYear()
-    
-#Retourne les primes complémentaires commerciales annuelles
-    def premiumCompl(self):     
-        
-        riderPremium=self.p['POLPRCPL9'].to_numpy()[:,np.newaxis,np.newaxis] * self.one()     
-        
-        return riderPremium
-
-#Retourne les primes pures annuelles  
-    def premiumPure(self):
-
-        annPrem = self.premiumAnnual()      
-        premRider = self.premiumCompl()
-  
-        #Determine les frais d'acquisition en fonction de l'année du contrat (A CHANGER CAR ON AURA SUREMENT DES CHGT SUR 1 2 ou 3 ANS pour d'autres polices)
-        # acquisitionLoading = (self.durationIf()<=12) * self.p['aquisitionLoading'].to_numpy()[:,np.newaxis,np.newaxis] * self.one()
-        acquisitionLoading=self.one()
-        acquisitionLoading =acquisitionLoading* (self.durationIf()<=12) * self.p['aquisitionLoading'].to_numpy()[:,np.newaxis,np.newaxis]
-        acquisitionLoading =acquisitionLoading* (self.durationIf()<=24) * self.p['aquisitionLoadingYear2'].to_numpy()[:,np.newaxis,np.newaxis]
-        acquisitionLoading =acquisitionLoading* (self.durationIf()<=36) * self.p['aquisitionLoadingYear3'].to_numpy()[:,np.newaxis,np.newaxis]
-        acquisitionLoading =acquisitionLoading* (self.durationIf()>36) * self.zero()
-                     
-        gestionLoading = self.p['gestionLoading'].to_numpy()[:,np.newaxis,np.newaxis] * self.one()
-
-        return (annPrem -  premRider)*(1-gestionLoading -acquisitionLoading )
-
-#Retourne les primes investis
-    def premiumInvested(self):
-        return self.isPremPay() * self.premiumPure()/self.frac()
-
-#Retourn la réserve mathémathique incluant la PB
-    def mathresBA(self):
-
-        return  self.epargnAcquPP + self.pbAcquPP + self.riskEnCours()      
-
-#Retourne le risque en cours
-    def riskEnCours(self):
-
-        #Age limite pour Epargne
-        agelimite=((self.age()-1)<=self.ageLimite)* self.one()
-
-        frek=self.frac()
-          
-        premCompl =  self.premiumCompl()/frek 
-        
-        #Calcul du risque en cours
-        riderIncPP=premCompl*agelimite*self.isPremPay()
-        riderIncPP2=premCompl*agelimite
-        
-        precPP = self.zero()
-
-        for i in range(1,self.shape[1]):
-            precPP[:,i,:]=precPP[:,i-1,:]+riderIncPP[:,i,:] - ((frek[:,i,:]/12)*riderIncPP2[:,i,:])
-              
-        return precPP
-
-#Retourne les claims décès
-    def deathClaim(self):
-        
-        addSumAssuree = self.p['POLCAPAUT'].to_numpy()[:,np.newaxis,np.newaxis] * self.one()
-        
-        deathBenefit = self.pbAcquPP + self.epargnAcquPP + addSumAssuree
-        
-        deathBenefitReduced=self.epAcquAVPUP + self.pbAcquAVPUP
-        
-        deathClaim = deathBenefit * self.nbrDeath + deathBenefitReduced * self.nbrPupDeath
-        
-        return deathClaim
-
-
 #Retourne les claims de la garantie principale (DEATH_OUTGO)
     def claimPrincipal(self):
-        return self.deathClaim()
+        return self.zero()
 
 #Retourne les claims des garanties complémentaires (RIDERC_OUTGO)
     def claimCompl(self):
@@ -262,14 +104,11 @@ class EP(Portfolio):
 
 #Retourne les rachats totaux (SURR_OUTGO)
     def surrender(self):
-        
-        #Rachats des polices active
-        surrIf = self.mathresBA() * self.nbrSurrender
-        
-        #Rachat des polices réduites
-        surrRed = (self.pbAcquAPPUP + self.eppAcquAPPUP) * self.nbrPupSurrender        
-        
-        return surrIf + surrRed
+        return self.zero()
+
+#Retourne les rachats partiels (PARTSV_OUTGO)
+    def partialSurrender(self):
+        return self.zero()
 
 #Retourne les échéances (MAT_OUTGO)
     def maturity(self):
@@ -284,17 +123,15 @@ class EP(Portfolio):
 def tester(self):
     return self
 
-pol = EP()
-#pol=EP(run=[4,5])
+pol = MI()
+#pol=MI(run=[4,5])
 # pol.ids([363001])
-# pol.ids([1900401])
-# pol.ids([1945101])
-# pol.ids([515503,1736301,1900401,2168101,2396001,2500001,2500101,2466301])
 
-pol.mod([28])
+
+pol.mod([6,7])
 #pol.modHead([9],2)
 aa = pol.p
-#a=pol.nbrPolIf
+# a=pol.nbrPolIf
 #b=pol.nbrPolIfSM
 #c=pol.nbrMaturities
 #d=pol.nbrDeath
@@ -308,7 +145,7 @@ aa = pol.p
 # l=pol.adjustedReserve()
 #m=pol.reserveExpense()
 #n=pol.unitExpense()
-# o=pol.totalPremium()
+o=pol.totalPremium()
 # q=pol.totalClaim()
 # r=pol.totalCommissions()
 # s=pol.totalExpense()
@@ -317,14 +154,11 @@ aa = pol.p
 # bel=np.sum(pol.BEL(), axis=0)
 # pgg=pol.PGG()
 
-gg=pol.claimPrincipal()
 
 
+print("Class MI--- %s sec" %'%.2f'%  (time.time() - start_time))
 
-
-print("Class EP--- %s sec" %'%.2f'%  (time.time() - start_time))
-
-monCas=pol.surrender()
+monCas=o
 
 zz=np.sum(monCas, axis=0)
 zzz=np.sum(zz[:,0])
