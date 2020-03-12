256c798a60f8985b10535010a9a4407e3f9685fb
robinWengi <52786448+robinWengi@users.noreply.github.com>
2020-03-12 15:54:40 +0100

    rw

--- Tests/test_PGG.py
+++ Tests/test_PGG.py
@@ -7,30 +7,30 @@ path = os.path.dirname(os.path.abspath(__file__))
 from MyPyliferisk import MortalityTable,qx
 from MyPyliferisk.mortalitytables import EKM05i
 
-from Portefeuille import Portfolio, portfolioPreProcessing
-from Parametres import Hypo
+from Portefeuille import Portfolio
+from Parametres import Hypo, portfolioPreProcessing
 from Produits import FU
 
 
 #Importation des données pour les tests
 
 #L'ensemble des variables Prophet par produit
-DataProphet=pd.read_excel(path+'\Tests_RW\Resultats_Prophet.xls',sheet_name=None,skiprows=7)
+DataProphet=pd.read_excel(path+'\Resultats_Prophet.xls',sheet_name=None,skiprows=7)
 DataProphet=pd.concat(DataProphet,axis=1)
 
 
 #Les resultats de la PGG selon la répartition en vigueur
-ResultatPGG=pd.read_excel(path+'\Tests_RW\Resultats_PGG.xls',sheet_name='Synthèse',skiprows=3)
+ResultatPGG=pd.read_excel(path+'\Resultats_PGG.xls',sheet_name='Synthese',skiprows=3)
 
 #Les variables de contrôle
-variablesTest=pd.read_excel(path+'\Tests_RW\Resultats_PGG.xls',sheet_name='AllVariablesTest',skiprows=7)
+variablesTest=pd.read_excel(path+'\Resultats_PGG.xls',sheet_name='AllVariablesTest',skiprows=7)
 
 #Portefeuille servant de test qui correspond on portefeuille au 31.12.2018
 pTest=pd.read_csv(path+'\Portfolio_Test.csv')
 pTest=portfolioPreProcessing(pTest)
 
 #Hypothèses servant de test qui correspond à celles en vigueur au 31.12.2018
-hTest=pd.ExcelFile(path  + '/TablesProphet 2018-12_Test.xls').parse("Hypothèses")
+hTest=pd.ExcelFile(path  + '/TablesProphet 2018-12_Test.xls').parse("Hypotheses")
 
 
 #Instenciation Des Class pour les tests
